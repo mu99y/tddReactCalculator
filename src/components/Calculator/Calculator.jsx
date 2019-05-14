a8958f9e6b201042a1db8357a28ba6cdc705bbee
Murugan Amir <murugan.learning@gmail.com>
2019-05-14 23:16:01 +0100

    Initial commit :sparkles: with TDD :joy: all the way to render Display component

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Display from '../Display/Display';
+import './Calculator.css';
+
+class Calculator extends Component {
+    state = {
+        displayValue: '0',
+        numbers: [],
+        operators: [],
+        selectedOperator: '',
+        storedValue: ''
+    }
+
+    callOperator = () => {
+        console.log('call operation');
+    }
+
+    setOperator = () => {
+        console.log('set operation');
+    }
+
+    updateDisplay = () => {
+        console.log('update display')
+    }
+
+    render = () => {
+
+        const { displayValue } = this.state;
+        return(
+            <div className="calculator-container" >
+                <Display displayValue={displayValue} />
+            </div>
+        );
+    }
+}
+
+export default Calculator ;
