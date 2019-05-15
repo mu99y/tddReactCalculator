d1a3770ca52db91297c51c6b05376297cdeee250
Murugan Amir <murugan.learning@gmail.com>
2019-05-15 23:59:52 +0100

    Add Keypad and keycomponent

--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import Key from '../Key/Key';
+import './Keypad.css';
 
-const Keypad = (
-    { callOperator, numbers, operators, setOperator, updateDisplay }
-) => <div className='keyboard-container' />;
+const Keypad = ({ 
+    callOperator, 
+    numbers, 
+    operators, 
+    setOperator, 
+    updateDisplay 
+}) => {
+    const numberKeys = numbers.map(number => <p key={number}>{number}</p>);
+    const operatorKeys = operators.map(operator => <p key={operator}>{operator}</p>);
+    return(
+        <div className='keyboard-container'>
+            <div className='numbers-container'>
+                {numberKeys}
+            </div>
+            <div className='operators-container'>
+                {operatorKeys}
+            </div>
+            <Key
+                keyAction={callOperator}
+                keyType=""
+                keyValue=""
+            />
+        </div>
+    );
+};
 
 Keypad.propTypes = {
     callOperator: PropTypes.func.isRequired,
--- src/components/Keypad/Keypad.spec.js
+++ src/components/Keypad/Keypad.spec.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { shallow } from 'enzyme';
 import Keypad from '../Keypad/Keypad';
+import Key from '../Key/Key';
 
-describe('', () => {
+describe('<Keypad />', () => {
     let wrapper;
 
     beforeEach(() => {
@@ -18,7 +19,21 @@ describe('', () => {
     });
 
     it('should render a <div />', () => {
-        expect(wrapper.find('div').length).toEqual(1);
+        expect(wrapper.find('div').length).toEqual(3);
     });
+
+    it('renders the values of numbers', () => {
+        wrapper.setProps({numbers: ['0', '1', '2']});
+        expect(wrapper.find('.numbers-container').text()).toEqual('012');
+    })
+    
+    it('renders the values of operators', () => {
+        wrapper.setProps({operators: ['+', '-', '*', '/']});
+        expect(wrapper.find('.operators-container').text()).toEqual('+-*/');
+    })
+    
+    it('should render an instance of the Key component', () => {
+      expect(wrapper.find('Key').length).toEqual(1);
+    })
     
 });
